d675033cc3f4aa3bf47bdf1dd6bf0e37282d0565
aki <60290657+ShojiAkifumi@users.noreply.github.com>
2022-09-30 08:42:04 +0900

    まい　ふぁーすと　こみっと

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,13 @@ import Layout from "components/layout";
 // Font Awesomeの設定
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import { usePageView, GoogleAnalytics } from "lib/gtag";
+import { usePageView } from "lib/gtag";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePageView();
   return (
     <>
-      <GoogleAnalytics />
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from "lib/gtag";
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <GoogleAnalytics />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
           rel="stylesheet"
