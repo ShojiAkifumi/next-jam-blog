decd0e4ac742146bac34f210d082674c671384c7
aki <60290657+ShojiAkifumi@users.noreply.github.com>
2022-09-30 07:17:04 +0900

    まい　ふぁーすと　こみっと

---
+++ src/components/accordion.tsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react";
+import styles from "styles/accordion.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleChevronDown } from "@fortawesome/free-solid-svg-icons";
+type Props = {
+  heading: string;
+  children: React.ReactNode;
+};
+
+const Accordion = ({ heading, children }: Props) => {
+  const [isTextOpen, setIsTextOpen] = useState(false);
+
+  const toggleText = () => {
+    setIsTextOpen((prev) => !prev);
+  };
+
+  return (
+    <div className={isTextOpen ? styles.open : styles.close}>
+      <h3 className={styles.heading}>
+        <button onClick={toggleText}>
+          {heading}
+          <FontAwesomeIcon icon={faCircleChevronDown} className={styles.icon} />
+        </button>
+      </h3>
+      <div className={styles.text}>
+        <div className={styles.textInner}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
