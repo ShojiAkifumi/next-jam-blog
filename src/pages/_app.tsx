a05b80c20798162044d829a5bf6495fbdb4c1cc5
aki <60290657+ShojiAkifumi@users.noreply.github.com>
2022-09-30 08:17:48 +0900

    まい　ふぁーすと　こみっと

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,18 @@ import Layout from "components/layout";
 // Font Awesomeの設定
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { usePageView, GoogleAnalytics } from "lib/gtag";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GoogleAnalytics />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
